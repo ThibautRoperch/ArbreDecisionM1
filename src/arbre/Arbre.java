a312f3d0468696f0aaa3ed5368401614792281ad
PierreGranier <piergranier@laposte.net>
2017-01-12 12:10:02 +0100

    cor arbre.java

--- src/arbre/Arbre.java
+++ src/arbre/Arbre.java
@@ -3,12 +3,20 @@ package arbre;
 import java.io.*;
 
 class Arbre {
-    protected Noeud noeud_racine;
-    protected Noeud fils1;
-    protected Noeud fils2;
+    protected Noeud noeud_racine; //faire les tests pour déterminer si le noeud est une feuille pure
+    protected Arbre fils1;
+    protected Arbre fils2;
 
     Arbre() {
         //calculer le meilleur attribut à mettre en noeud racine, puis remplir les fils. Puis recommencer sur les fils.
 
     } 
+
+    //Calcule le gain pour choisir le meilleur attribut.
+    //Prendre le jeu d'apprentissage en paramètre maybe ? 
+    int gain(int positif, int negatif) {
+        //return positif*(log(positif/positif+negatif)-log(P/P+N));
+
+        //Appeler la fonction pour chaque attribut, et mettre à jour le
+    }
 }
