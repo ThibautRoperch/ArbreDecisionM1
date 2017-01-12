122a995257fbbda1de7b2709dfe54fa95477e7ef
PierreGranier <piergranier@laposte.net>
2017-01-12 11:41:30 +0100

    créa de arbre.java et noeud.java

---
+++ src/Arbre.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Arbre [
+    protected Noeud noeud_racine;
+    protected Noeud fils1;
+    protected Noeud fils2;
+
+    Arbre() {
+        //calculer le meilleur attribut à mettre en noeud racine, puis remplir les fils. Puis recommencer sur les fils.
+
+    } 
+]
+
---
+++ src/Noeud.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Noeud {
+
+    protected int N;        //Taille de la matrice, on la sort de chais pas trop où 
+    protected String[N][N];
+
+    Noeud() {
+        
+    }
+}
